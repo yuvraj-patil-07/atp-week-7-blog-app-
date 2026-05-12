328223aed2e30292b9fdabb5bdc1a89149981d88
yuvraj-patil-07 <yuvrajpatil2806@gmail.com>
2026-05-12 14:35:53 +0530

    Update AuthorArticles.jsx

--- frontend/src/components/AuthorArticles.jsx
+++ frontend/src/components/AuthorArticles.jsx
@@ -33,7 +33,7 @@ function AuthorArticles() {
       try {
         setLoading(true);
         //read articles of current author
-        let res = await axios.get(https://atp-week-7-blog-app-1.onrender.com/author-api/articles`, { withCredentials: true });
+        let res = await axios.get(`https://atp-week-7-blog-app-1.onrender.com/author-api/articles`, { withCredentials: true });
         if (res.status === 200) {
           setArticles(res.data.payload);
         }
