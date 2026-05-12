a1cc2d79ef7d6e18600b4347f4157675d52fd6da
yuvraj-patil-07 <yuvrajpatil2806@gmail.com>
2026-05-12 14:29:47 +0530

    Update API URL for fetching user articles

--- frontend/src/components/UserProfile.jsx
+++ frontend/src/components/UserProfile.jsx
@@ -28,7 +28,7 @@ function UserProfile() {
       setLoading(true);
       try {
         //read articles of all authors
-        let res=await axios.get(`${import.meta.env.VITE_API_BASE_URL}/user-api/articles`,{withCredentials:true})
+        let res=await axios.get(`https://atp-week-7-blog-app-1.onrender.com/user-api/articles`,{withCredentials:true})
         //update articles state
         if(res.status===200){
           setArticles((await res).data.payload)
